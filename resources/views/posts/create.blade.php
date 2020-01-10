64bec13aefaf3b94cdf420586fbfb0d93a94f02e
jeetg57 <jeetg57@gmail.com>
2020-01-10 17:01:30 +0300

    posts visible on index

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,38 +2,37 @@
 
 @section('content')
 <div class="container">
-    <form action="">
+    <form action="/p" enctype="multipart/form-data" method="post">
+        @csrf
         <div class="row">
-        <div class="col-8 offset-2">
-            <div class="row">
-                <h1>Add New Post</h1>
-            </div>
-        <div class="form-group row">
-            <label for="caption" class="col-md-4 col-form-label">{{ __('Caption') }}</label>
+            <div class="col-8 offset-2">
+                <div class="row">
+                    <h1>Add New Post</h1>
+                </div>
+                <div class="form-group row">
+                    <label for="caption" class="col-md-4 col-form-label">{{ __('Caption') }}</label>
+                    <input id="caption" name="caption" type="text"
+                        class="form-control @error('caption') is-invalid @enderror" caption="caption"
+                        value="{{ old('caption') }}" autocomplete="caption" autofocus>
 
-            
-                <input id="caption" type="text" class="form-control @error('caption') is-invalid @enderror" caption="caption" value="{{ old('caption') }}"  autocomplete="caption" autofocus>
-
-                @error('caption')
+                    @error('caption')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                @enderror
-           
-        </div>
-            <div class="form-group row">
-                <label for="image" class="col-md-4 col-form-label">{{ __('Image') }}</label>
-                <input type="file" name="image" id="image" class="form-control-file">
-                @error('image')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                    @enderror
+
+                </div>
+                <div class="form-group row">
+                    <label for="image" class="col-md-4 col-form-label">{{ __('Image') }}</label>
+                    <input type="file" name="image" id="image" class="form-control-file">
+                    @error('image')
+                    <strong style="color: #e3342f; font-size: 80%;">{{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="row pt-4">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
             </div>
-            <div class="row pt-4">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-        </div>
         </div>
     </form>
 </div>
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -2,34 +2,34 @@
 
 @section('content')
 <div class="container">
-   <div class="row">
-       <div class="col-3 p-5">
-           <img src="https://images.pexels.com/photos/238620/pexels-photo-238620.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="rounded-circle w-100" alt="">
-       </div>
-       <div class="col-9 pt-5">
-           <div class="d-flex justify-content-between align-items-baseline">
-               
-           <h1>{{ $user->username }}</h1>
-           <a href="#" class="btn btn-primary">Add New Post</a>
-           </div>
-           <div class="d-flex">
-               <div class="pr-5"><strong>253</strong> posts</div>
-               <div class="pr-5"><strong>23k</strong> followers</div>
-               <div class="pr-5"><strong>212</strong> following</div>
-           </div>
-        <div class="pt-4 font-weight-bold">{{ $user->profile->title}}</div>
-           <div>
-            {{$user->profile->description}}
-           </div>
-        <div><a href="#">{{$user->profile->url }}</a></div>
-       </div>
-   </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://images.pexels.com/photos/238620/pexels-photo-238620.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                class="rounded-circle w-100" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
 
-   <div class="row pt-5">
-     <div class="col-4"><img src="https://images.pexels.com/photos/3507989/pexels-photo-3507989.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="w-100" alt=""></div>
-     <div class="col-4"><img src="https://images.pexels.com/photos/3467153/pexels-photo-3467153.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="w-100" alt=""></div>
-     <div class="col-4"><img src="https://images.pexels.com/photos/3489072/pexels-photo-3489072.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="w-100" alt=""></div>
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create" class="btn btn-primary">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title}}</div>
+            <div>
+                {{$user->profile->description}}
+            </div>
+            <div><a href="#">{{$user->profile->url }}</a></div>
+        </div>
+    </div>
 
-   </div>
+    <div class="row pt-5">
+        @foreach ($user->posts as $post)
+        <div class="col-4 pb-4"><img src="/storage/{{$post->image}}" class="w-100" alt=""></div>
+        @endforeach
+    </div>
 </div>
 @endsection
